e8301cedb8769959649916f119d702a80d1f3084
NatureZhang <895234387@qq.com>
2017-01-20 19:31:11 +0800

    BinTree

---
+++ ReviewC/BinTree.c
@@ -0,0 +1,12 @@
+//
+//  BinTree.c
+//  ReviewC
+//
+//  Created by zhangdong on 2017/1/20.
+//  Copyright © 2017年 __Nature__. All rights reserved.
+//
+
+/// 二叉树相关的知识
+/// http://blog.csdn.net/fansongy/article/details/6798278  这篇文章写得不错
+
+#include "BinTree.h"
---
+++ ReviewC/BinTree.h
@@ -0,0 +1,14 @@
+//
+//  BinTree.h
+//  ReviewC
+//
+//  Created by zhangdong on 2017/1/20.
+//  Copyright © 2017年 __Nature__. All rights reserved.
+//
+
+#ifndef BinTree_h
+#define BinTree_h
+
+#include <stdio.h>
+
+#endif /* BinTree_h */
